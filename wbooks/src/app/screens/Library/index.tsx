8623d3f8130c6f41edfe96083426b92bd0ed97ee
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-12-23 11:45:58 -0300

    create custom functions to renderitem and keyExtractor

--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, ListRenderItemInfo, View } from 'react-native';
 import { BOOKS_MOCK } from '@constants/mockBooks';
+import { IBook } from '@interfaces/book';
 
 import Book from './components/Book';
 import { styles } from './styles';
 
-const Library = () => (
-  <View>
-    <FlatList
-      data={BOOKS_MOCK}
-      // TODO: Move this to some layout component in card of library view
-      style={styles.list}
-      renderItem={({ item: { imageUrl, title, author } }) => (
-        <Book imageUrl={imageUrl} title={title} author={author} />
-      )}
-      keyExtractor={({ id }) => id.toString()}
-    />
-  </View>
-);
+function Library() {
+  const handleRenderItem = ({ item: { imageUrl, title, author } }: ListRenderItemInfo<IBook>) => (
+    <Book imageUrl={imageUrl} title={title} author={author} />
+  );
+
+  const handleKeyExtractor = ({ id }: IBook) => id.toString();
+
+  return (
+    <View>
+      <FlatList
+        data={BOOKS_MOCK as IBook[]}
+        // TODO: Move this to some layout component in card of library view
+        style={styles.list}
+        renderItem={handleRenderItem}
+        keyExtractor={handleKeyExtractor}
+      />
+    </View>
+  );
+}
 
 export default Library;
