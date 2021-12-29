0cfca534a7c8660feb779c48d0b650a20b9a5fb8
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-12-29 15:47:50 -0300

    add IMG_SQUARE_SIZE

--- wbooks/src/app/screens/Library/screens/BookDetail/components/Comment/styles.ts
+++ wbooks/src/app/screens/Library/screens/BookDetail/components/Comment/styles.ts
@@ -1,14 +1,16 @@
 import { AppColors } from '@constants/colors';
 import { StyleSheet } from 'react-native';
 
+const IMG_SQUARE_SIZE = 45;
+
 export const styles = StyleSheet.create({
   comment: {
     flexDirection: 'row',
     paddingTop: 15
   },
   image: {
-    width: 45,
-    height: 45,
+    width: IMG_SQUARE_SIZE,
+    height: IMG_SQUARE_SIZE,
     borderRadius: 50
   },
   data: {
