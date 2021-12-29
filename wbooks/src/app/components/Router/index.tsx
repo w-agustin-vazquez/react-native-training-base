4b1cb2a9e2bc6fff53bd08a0319f7c4f6bc44f77
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-12-29 11:52:09 -0300

    add styles to header

--- wbooks/src/app/components/Router/index.tsx
+++ wbooks/src/app/components/Router/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppColors } from '@constants/colors';
 
 import { DEFAULT_MAIN_ROUTE, ROUTES } from './constants';
+import { styles } from './styles';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +12,18 @@ function Router() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={DEFAULT_MAIN_ROUTE}>
-        {ROUTES.map(({ name, component }) => (
-          <Stack.Screen key={name} name={name} component={component} />
+        {ROUTES.map(({ name, component, title }) => (
+          <Stack.Screen
+            key={name}
+            name={name}
+            component={component}
+            options={{
+              title,
+              headerTintColor: AppColors.white,
+              headerStyle: styles.header,
+              headerTitleStyle: styles.title
+            }}
+          />
         ))}
       </Stack.Navigator>
     </NavigationContainer>
---
+++ wbooks/src/app/components/Router/styles.ts
@@ -0,0 +1,11 @@
+import { AppColors } from '@constants/colors';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  header: {
+    backgroundColor: AppColors.cerulean
+  },
+  title: {
+    fontWeight: 'bold'
+  }
+});
