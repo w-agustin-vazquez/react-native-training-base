a4527413a5d6c78adb96bab7ae4646ca02e33f6a
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2021-12-29 15:52:15 -0300

    generate screenOptions

--- wbooks/src/app/components/Router/index.tsx
+++ wbooks/src/app/components/Router/index.tsx
@@ -9,21 +9,18 @@ import { styles } from './styles';
 const Stack = createNativeStackNavigator();
 
 function Router() {
+  const screenOptions = (title: string) => ({
+    title,
+    headerTintColor: AppColors.white,
+    headerStyle: styles.header,
+    headerTitleStyle: styles.title
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={DEFAULT_MAIN_ROUTE}>
         {ROUTES.map(({ name, component, title }) => (
-          <Stack.Screen
-            key={name}
-            name={name}
-            component={component}
-            options={{
-              title,
-              headerTintColor: AppColors.white,
-              headerStyle: styles.header,
-              headerTitleStyle: styles.title
-            }}
-          />
+          <Stack.Screen key={name} name={name} component={component} options={screenOptions(title)} />
         ))}
       </Stack.Navigator>
     </NavigationContainer>
