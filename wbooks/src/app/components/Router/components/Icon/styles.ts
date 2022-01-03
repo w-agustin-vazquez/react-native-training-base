ee6c102f94c3d4e5bdab6a77ca228d2521c9bcd0
Agustin Vazquez <agustin.vazquez@wolox.com.ar>
2022-01-03 12:22:31 -0300

    insert icon dimentions in a const

--- wbooks/src/app/components/Router/components/Icon/styles.ts
+++ wbooks/src/app/components/Router/components/Icon/styles.ts
@@ -1,10 +1,12 @@
 import { AppColors } from '@constants/colors';
 import { StyleSheet } from 'react-native';
 
+const ICON_SQUARE_SIZE = 30;
+
 export const styles = StyleSheet.create({
   icon: {
-    width: 30,
-    height: 30,
+    width: ICON_SQUARE_SIZE,
+    height: ICON_SQUARE_SIZE,
     tintColor: AppColors.dustyGray
   },
   active: {
